9fd2ad8b4f2b2b302b425398eac0e9da109987f8
Ingrid Bianca <ingridbianca10@gmail.com>
2018-04-18 12:43:48 -0300

    ngFor

---
+++ meu-jornal/src/app/noticia/noticia.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticia',
+  templateUrl: './noticia.component.html',
+  styleUrls: ['./noticia.component.css']
+})
+export class NoticiaComponent implements OnInit {
+
+  //noticias: Array<string>; ou
+  noticias: string[] = ["Noticia 1","Noticia 2","Noticia 3", "Noticia 4"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
