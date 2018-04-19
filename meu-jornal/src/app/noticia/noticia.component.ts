b0d1b500eccce792a05abfdc551f3f82cbf78b27
Ingrid Bianca <ingridbianca10@gmail.com>
2018-04-19 15:18:54 -0300

    Two Way Binding

--- meu-jornal/src/app/noticia/noticia.component.ts
+++ meu-jornal/src/app/noticia/noticia.component.ts
@@ -10,7 +10,10 @@ export class NoticiaComponent implements OnInit {
   //noticias: Array<string>; ou
   noticias: string[] = ["Noticia 1","Noticia 2","Noticia 3", "Noticia 4"];
   mostrarNoticia: boolean = true;
-  
+  titulo: string = 'Curso Angular';
+  texto: string = 'Utilizando String Interpolation';
+  caminho_imagem: string = '../../assets/princesa.png';
+    
   constructor() { }
 
   ngOnInit() {
